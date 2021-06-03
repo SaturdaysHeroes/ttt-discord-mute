9347ac9f823cb1f3f384df2e453f2bf019ea231c
Oskar <saturdaysheroes.business@gmail.com>
2021-06-03 18:47:50 +0200

    start server

--- discord/src/index.ts
+++ discord/src/index.ts
@@ -1,12 +1,14 @@
 import * as dotenv from "dotenv"; dotenv.config();
 import * as Discord from "discord.js";
 import {default as StartBot} from "./bot";
+import {default as StartServer} from "./server";
 
 // Initialize the Discord clien. 
 const client = new Discord.Client(); 
 
 // Initialize the Bot and Server. 
 StartBot(client); 
+StartServer(client);
 
 process.on("unhandledRejection", err => {
     throw err;
